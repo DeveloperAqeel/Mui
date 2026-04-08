54d978d29357506ff7f37a63e91cf527bfdfabff
DeveloperAqeel <aqeel471860438@gmail.com>
2026-04-07 23:52:24 -0700

    working on !

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Button from '@mui/material/Button';
+import Button2 from './commponents/button2';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Button variant="contained" color="primary">
+      button with using material ui
+    </Button>
+     <h1>Hello Aqeel !</h1>
+     <Button2 />
+    </>
+  )
+}
+
+export default App
---
+++ src/commponents/button2.jsx
@@ -0,0 +1,9 @@
+import  Button  from 'antd';
+
+const Button2 = () => {
+  return (
+    <Button type="primary">button with using Ant Design</Button>
+  );
+};
+
+export default Button2;
